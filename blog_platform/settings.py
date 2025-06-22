e82387063a6ffa34d4075a778aef9a0d9bdeae7d
Uttam singh <anshuchauhan1611@gmail.com>
2025-06-22 10:45:16 +0530

    Updated post list template and fixed delete functionality and also added postgreSQL as database

--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'blog_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'Ut12345',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
